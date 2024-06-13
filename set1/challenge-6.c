d711859ed7e50d276e518f64470c2320e0bef76e
Andrew Decker <ajdecker1022@gmail.com>
2024-06-13 12:35:26 -0500

    Write working hamming distance function

--- set1/challenge-6.c
+++ set1/challenge-6.c
@@ -1,19 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "encodings.h"
+#include "bytes.h"
 
 int main(int argc, char **argv)
 {
-    init_base64_module();
-    char *input = "V2hlcmV2ZXIgSSBnbywgdGhlcmUgSSBhbQ==";
-    char *output = base64_decode(input, 36);
-    printf("%s", output);
+    char ltest[] = "this is a test";
+    char rtest[] = "wokka wokka!!!";
 
+    printf("Hamming distance: %d\n", hamming_distance(ltest, rtest, 14));
+
+    init_base64_module();
 
 
     // Load file
 
     // Decode base64 into unencoded (but encrypted) bytes
+
+    // TODO - Write better english score
 
 
     return 0;
