a1a18d27d3a5f26b11eb34e0d551b4067126f447
Andrew Decker <ajdecker1022@gmail.com>
2024-06-15 12:16:18 -0500

    Get build working

--- set1/challenge-7.c
+++ set1/challenge-7.c
@@ -3,7 +3,11 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/stat.h>
+
 #include <openssl/evp.h>
+#include <openssl/err.h>
+
+#include "../common/encodings.h"
 
 int main(int argc, char **argv)
 {
@@ -15,9 +19,7 @@ int main(int argc, char **argv)
         goto error;
 
     struct stat file_stat;
-    
-    
-    if (stat(filename, &stat) == -1)
+    if (stat(filename, &file_stat) == -1)
         goto error;
     
     char *buf = malloc(file_stat.st_size);
@@ -25,18 +27,41 @@ int main(int argc, char **argv)
         goto error;
 
     if (!fread(buf, 1, file_stat.st_size, challenge_file))
-
+        goto error;
 
     init_base64_module();
     char *encrypted_input = base64_decode(buf, file_stat.st_size);
     size_t encrypted_input_size = 3 * file_stat.st_size / 4;
     
     char key[16] = "YELLOW SUBMARINE";
-    EVP_CIPHER* cipher = EVP_CIPHER_fetch(NULL, "", NULL);
-    
+
+    // The initial value for the cipher
+    char iv[16] = "0123456789abcdef";
+
+    // AES-128-ECB
+    EVP_CIPHER* cipher = EVP_CIPHER_fetch(NULL, "asdf", NULL);
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    if (ctx == NULL)
+    if (ctx == NULL || cipher == NULL)
         goto error;
+
+    // Make sure the key length and iv length match our expectation    
+    OPENSSL_assert(EVP_CIPHER_CTX_get_key_length(ctx) == 16);
+    OPENSSL_assert(EVP_CIPHER_CTX_get_iv_length(ctx) == 16);
+
+    unsigned char *outbuf;
+    int outlen;
+
+    // Last parameter set to 0 to indicate decryption (rather than 1 for encryption)
+    if (!EVP_CipherInit_ex2(ctx, cipher, key, iv, 0, NULL))
+        goto error;
+
+    if (!EVP_CipherUpdate(ctx, outbuf, &outlen, encrypted_input, encrypted_input_size))
+        goto error;
+    
+    if (!EVP_CipherFinal_ex(ctx, outbuf, &outlen))
+        goto error;
+    
+    printf("%s", outbuf);
 
     ossl_ret = 0;
 
